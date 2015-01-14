f78a99f83c314fb88255b016cce1c7928fa56886
Alex <alexey94709470@gmail.com>
2015-01-14 13:45:06 +0300

    Another fail

--- ReformaAPITesting/Program.cs
+++ ReformaAPITesting/Program.cs
@@ -1,64 +1,122 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using ReformaAPITesting.ReformaAPI;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.ServiceModel.Dispatcher;
-using System.IdentityModel.Selectors;
-using Microsoft.ServiceBus;
+using System.ServiceModel.Configuration;
 
 namespace ReformaAPITesting
 {
-    public class CustomToken 
+    public static class SessionKey 
     {
-        public string SecurityKey { get; set; }
+        public static string LogKey { get; set; } 
+    }
+    
+    /*
+    #region Новый вариант 2
+    public class InterceptorMessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref Message reply, object correlationState) 
+        {
+            Console.WriteLine(reply.Headers.Count);
+            Console.WriteLine("!!!!!!!!!!!!!!!!");
+        }
 
-        public CustomToken(string key) {
-            SecurityKey = key;
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            throw new NotImplementedException();
         }
     }
 
-    public class AuthHeaderBehaviour : IEndpointBehavior, IClientMessageInspector
+    public class InterceptorBehaviour : IEndpointBehavior 
     {
-        private readonly CustomToken tokenProvider;
-
-        public AuthHeaderBehaviour(CustomToken tokenProvider) 
+        public void ApplyClientBehaviour(ServiceEndpoint endpoint, ClientRuntime clientRuntime) 
         {
-            this.tokenProvider = tokenProvider;
+            clientRuntime.MessageInspectors.Add(new InterceptorMessageInspector());
         }
 
         public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
         {
-        }
-
-        public void AfterReceiveReply(ref Message reply, object correlationState)
-        {
+            throw new NotImplementedException();
         }
 
         public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
         {
-            clientRuntime.MessageInspectors.Add(this);
+            throw new NotImplementedException();
         }
-        
+
         public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
         {
-
+            throw new NotImplementedException();
         }
 
-        public object BeforeSendRequest(ref Message request, IClientChannel clientChannel) {
-            request.Headers.Add(MessageHeader.CreateHeader("authenticate", string.Empty, this.tokenProvider.SecurityKey));
-            return null;
-        }
-        
         public void Validate(ServiceEndpoint endpoint)
         {
-
+            throw new NotImplementedException();
         }
     }
+    #endregion
 
+    #region Новый вариант
+    public class MyBehaviour : BehaviorExtensionElement, IEndpointBehavior
+    {
+        private string headerKey = "authenticate";
+        public MyBehaviour() { }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.MessageInspectors.Add(new MyInspector());
+        }
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            endpointDispatcher.DispatchRuntime.MessageInspectors.Add(new MyInspector());
+        }
+
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+        public void Validate(ServiceEndpoint endpoint) { }
+
+        protected override object CreateBehavior()
+        {
+            return new MyBehaviour();
+        }
+
+        public override Type BehaviorType
+        {
+            get
+            {
+                return typeof(MyBehaviour);
+            }
+        }
+        /*public void AfterReceiveReply(ref Message reply, object correlationState) { }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            request.Headers.Add(MessageHeader.CreateHeader(headerKey, string.Empty, SessionKey.LogKey));
+            return null;
+        }*/
+    }
+
+    public class MyInspector : IClientMessageInspector, IDispatchMessageInspector
+    {
+        private const string headerKey = "authenticate";
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            request.Headers.Add(MessageHeader.CreateHeader(headerKey, string.Empty, SessionKey.LogKey));
+            return null;
+        }
+
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            return null;
+        }
+
+        public void AfterReceiveReply(ref Message reply, object correlationState) { }
+        public void BeforeSendReply(ref Message reply, object correlationState) { }
+    }
+    #endregion
+    */
     public class Program
     {
         /*
@@ -77,8 +135,10 @@ namespace ReformaAPITesting
          * http://blogs.msdn.com/b/stcheng/archive/2009/02/21/wcf-how-to-inspect-and-modify-wcf-message-via-custom-messageinspector.aspx
          * http://msdn.microsoft.com/en-us/library/ms733786(v=vs.110).aspx
          * 
+         * Custom endpoint behaviour
          * http://stackoverflow.com/questions/10448327/edit-soap-of-a-wcf-service-using-iclientmessageinspector  ???
-        */
+         * http://burcakcakiroglu.com/defining-custom-wcf-endpoint-behavior/
+         */
         public static string LOGIN = "a.zhelepov";
         public static string PASSWORD = "!ALEX!()$!(($!";
         public static string endpointAddress = "http://api-beta.reformagkh.ru/api_document_literal";
@@ -92,47 +152,32 @@ namespace ReformaAPITesting
             RequestState[] states;
             FiasAddress address = new ReformaAPI.FiasAddress();
             ApiSoapPortClient client = new ApiSoapPortClient(new BasicHttpBinding("ApiSoapBinding"), new EndpointAddress(endpointAddress));
-
             try
             {
                 Console.WriteLine("Before connection status: " + client.State.ToString());
                 response.LoginResult = client.Login(LOGIN, PASSWORD);
+                SessionKey.LogKey = response.LoginResult; 
                 Console.WriteLine("After connection status: " + client.State.ToString());
             }
             catch (Exception e)
             {
-                Console.WriteLine("***********************");
-                Console.WriteLine(client.InnerChannel.LocalAddress.Uri.Host);
+                Console.WriteLine("**********Login exception*************");
                 Console.WriteLine(e.Message);
-                Console.WriteLine("***********************");
+                Console.WriteLine("**************************************");
             }
 
-            CustomToken token = new CustomToken(response.LoginResult);
-            AuthHeaderBehaviour behaviour = new AuthHeaderBehaviour(token);
-            client.Endpoint.Behaviors.Add(new AuthHeaderBehaviour(token));
-            //client.SetRequestForSubmit(new string[] { "7329012644" });
-
-
-            Console.WriteLine("Login result: " + response.LoginResult);
-            Console.WriteLine("URI - " + client.Endpoint.Address.Uri.AbsoluteUri);
-            var cridential = new System.Net.NetworkCredential(request.login, request.password);
+            //client.Endpoint.Behaviors.Insert(0, new MyBehaviour());
+            InterceptorBehaviour b = new InterceptorBehaviour();
+            client.ChannelFactory.Endpoint.Behaviors.Insert(0, new InterceptorBehaviour());
             
-            Console.WriteLine("===================================");
-            foreach(var item in client.Endpoint.Behaviors) {
-                Console.WriteLine(item.ToString());
-            }
-
-            //client.Endpoint.Behaviors.Add();
-            //client.Endpoint.Behaviors.Add(new AuthHeaderBehaviour(TokenProvider.CreateOAuthTokenProvider(new [] { new Uri(endpointAddress) }, cridential)));
-
-            
-
-            /*
-            foreach (var item in client.Endpoint.Contract.Operations)
+            Console.WriteLine("Behaviours count = " + client.Endpoint.Behaviors.Count);
+            foreach (var item in client.Endpoint.Behaviors) 
             {
-                Console.WriteLine(item.Messages[0].Headers.ToString());
+                Console.WriteLine(item.GetType().Name);
+                //if (item.GetType().Name.Equals("MyBehaviour"))
+                    //item.
             }
-            */
+
             try
             {
                 statuses = client.SetRequestForSubmit(new string[] { "7329012644" });
@@ -160,3 +205,4 @@ namespace ReformaAPITesting
         }
     }
 }
+
